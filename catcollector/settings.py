2c4a3bb593c6016306f981afdcc6b5a3bb59e344
mahshidmn <ma.mojtabavi@gmail.com>
2024-04-15 15:18:57 +1000

    render debugging

--- catcollector/settings.py
+++ catcollector/settings.py
@@ -132,6 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
